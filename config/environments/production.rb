6c984ac83bb639ec39ee7d1d47e3449488feb247
3lviend <henryss_10@yahoo.com>
2021-10-27 14:19:07 +0700

    SMTP setup setting

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -7,9 +7,6 @@ Rails.application.configure do
     'http://localhost:3000',
     'https://sb-forum.herokuapp.com/'
   ]
-
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://ror-forum.herokuapp.com" }
   
   # Code is not reloaded between requests.
   config.cache_classes = true
@@ -65,6 +62,16 @@ Rails.application.configure do
 
   config.action_mailer.default_url_options = { host: 'ror-forum.herokuapp.com' }
   config.action_mailer.perform_caching = false
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: 'smtp.gmail.com',
+    port: 587,
+    domain: 'ror-forum.herokuapp.com',
+    user_name: 'elvin.alvian.siagian@gmail.com',
+    password: ENV['SMTP_PASSWORD'],
+    authentication: :plain,
+    enable_starttls_auto: true
+  }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -3,7 +3,7 @@ ActionMailer::Base.smtp_settings = {
   port: 587,
   domain: 'ror-forum.herokuapp.com',
   user_name: 'elvin.alvian.siagian@gmail.com',
-  password: ENV['SENDGRID_API_KEY'],
+  password: ENV['SMTP_PASSWORD'],
   authentication: :plain,
   enable_starttls_auto: true
 }
