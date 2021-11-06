37a2b5435d754d6f38390febfa77aa4953f3b937
3lviend <henryss_10@yahoo.com>
2021-11-06 14:30:31 +0700

    update ordering comment

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -4,7 +4,11 @@ class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User', foreign_key: 'user_id'
   belongs_to :post
   belongs_to :comment, optional: true
+
+  default_scope { order(created_at: :desc) }
+
   has_many :comments, dependent: :destroy
+
   validates :body, length: { in: 2..400 }, presence: true
 
   def comment_json
